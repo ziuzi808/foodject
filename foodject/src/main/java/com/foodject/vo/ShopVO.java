ab56ddbe3b7dcf91c030268a2744a047a13e785a
yunchanbin <21611273@yu.ac.kr>
2022-07-14 15:42:44 +0900

    Test complete

--- foodject/src/main/java/com/foodject/vo/ShopVO.java
+++ foodject/src/main/java/com/foodject/vo/ShopVO.java
@@ -1,5 +1,7 @@
 package com.foodject.vo;
 
+import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,12 +15,56 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ShopVO {
-	private String uid;
-	private String pwd;
+	private int id;
+	private String mid;
 	private String name;
+	private String bnum;
 	private String addr;
 	private String phon;
-	private String birth;
+	private String logo;
+	private String info;
+	private Date date;
+	private int allmin;
+	private int status;
+	
+	
+	
+	
+	//for updateSts (2)
+	public ShopVO(int id, int status) {
+		this.id = id;
+		this.status = status;
+	}
+
+	//for update (7)
+	public ShopVO(int id, String name, String addr, String phon, String logo, String info, int allmin) {
+		this.id = id;
+		this.name = name;
+		this.addr = addr;
+		this.phon = phon;
+		this.logo = logo;
+		this.info = info;
+		this.allmin = allmin;
+	}
+
+	//for insert (9)
+	public ShopVO(String mid, String name, String bnum, String addr, String phon, String logo, String info, Date date,
+			int allmin) {
+		this.mid = mid;
+		this.name = name;
+		this.bnum = bnum;
+		this.addr = addr;
+		this.phon = phon;
+		this.logo = logo;
+		this.info = info;
+		this.date = date;
+		this.allmin = allmin;
+	}
+	
+	
+	
+	
+	
 }
 
 
