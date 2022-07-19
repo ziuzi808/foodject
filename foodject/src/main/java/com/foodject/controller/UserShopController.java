0ff41b5367c18f21d0ce6c2f7cca1fc283e6f1bf
yunchanbin <21611273@yu.ac.kr>
2022-07-19 14:31:42 +0900

    selectMain: 음식점 목록 조회 기능 구현

--- foodject/src/main/java/com/foodject/controller/UserShopController.java
+++ foodject/src/main/java/com/foodject/controller/UserShopController.java
@@ -1,12 +1,23 @@
 package com.foodject.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.foodject.biz.UserShopBiz;
+import com.foodject.vo.MarkerVO;
+import com.foodject.vo.UserShopVO;
+
 @Controller
 @RequestMapping("/shop")
 public class UserShopController {
+	
+	@Autowired
+	UserShopBiz sbiz;  
+	
 	
 	public void mainProduct(Model m) {
 //		List<ProductVO> plist = null;
@@ -23,8 +34,18 @@ public class UserShopController {
 
 	@RequestMapping("")
 	public String main(Model m, int cid, double latt, double logt) {
+		MarkerVO obj = new MarkerVO(latt,logt,cid);
+		List<UserShopVO> list = null;
+		try {
+			list = sbiz.getMain(obj);
+			m.addAttribute("shoplist",list);
+			m.addAttribute("center", "user/shop/center");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		m.addAttribute("center", "user/shop/center");
+		
 		return "user/index";
 	}
 	
