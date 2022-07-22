fac38f7d6226949197d46c270fbf6624379b8a72
jin-aka <kayoungjin@gmail.com>
2022-07-22 17:28:26 +0900

    0722 user orders test

---
+++ foodject/src/main/java/com/foodject/biz/UserOrdersBiz.java
@@ -0,0 +1,44 @@
+package com.foodject.biz;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.foodject.frame.Biz;
+import com.foodject.mapper.UserOrdersMapper;
+import com.foodject.vo.UserOrdersVO;
+
+@Service
+public class UserOrdersBiz implements Biz<Integer, UserOrdersVO> {
+
+	@Autowired
+	UserOrdersMapper dao;
+	
+	@Override
+	public void register(UserOrdersVO v) throws Exception {
+		dao.insert(v);
+	}
+
+	@Override
+	public void modify(UserOrdersVO v) throws Exception {
+		dao.update(v);
+	}
+
+	@Override
+	public void remove(Integer k) throws Exception {
+		dao.delete(k);
+	}
+
+	@Override
+	public UserOrdersVO get(Integer k) throws Exception {
+		return dao.select(k);
+	}
+
+	@Override
+	public List<UserOrdersVO> get() throws Exception {
+		return dao.selectall();
+	}
+
+
+}
---
+++ foodject/src/main/java/com/foodject/vo/UserOrdersVO.java
@@ -0,0 +1,56 @@
+package com.foodject.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserOrdersVO {
+
+	private int id;
+	private String uid;
+	private int sid;
+	private String addr;
+	private String addrd;
+	private String phon;
+	private String nick;
+	private Date date;
+	private int status;
+	private String ask;
+	
+	public UserOrdersVO(int id, String uid, int sid, String addr, String addrd, String phon, String nick) {
+		this.id = id;
+		this.uid = uid;
+		this.sid = sid;
+		this.addr = addr;
+		this.addrd = addrd;
+		this.phon = phon;
+		this.nick = nick;
+	}
+	public UserOrdersVO(int id, String uid, int sid, String addr, String addrd, String phon, String nick,String ask) {
+		this.id = id;
+		this.uid = uid;
+		this.sid = sid;
+		this.addr = addr;
+		this.addrd = addrd;
+		this.phon = phon;
+		this.nick = nick;
+		this.ask = ask;
+	}
+	public UserOrdersVO(int id, int status) {
+		this.id = id;
+		this.status = status;
+	}
+	
+	
+	
+	
+}
